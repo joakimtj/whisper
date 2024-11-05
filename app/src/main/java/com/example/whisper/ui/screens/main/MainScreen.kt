e5e4bc4052984bc0dc0891a2f555cba203de7d25
joakimtj <joakimtj@hiof.no>
2024-11-05 14:34:35 +0100

    Remove some unused imports in MainScreen

--- app/src/main/java/com/example/whisper/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/whisper/ui/screens/main/MainScreen.kt
@@ -10,9 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CenterAlignedTopAppBar
